db6f079176d67f869fb9b3773fe24ff3d6a53a93
albfan <albertofanjul@gmail.com>
2018-03-17 12:05:50 +0100

    show included files

--- src/main/java/com/niranjanrao/GirToGnomeJava.java
+++ src/main/java/com/niranjanrao/GirToGnomeJava.java
@@ -71,7 +71,16 @@ public class GirToGnomeJava {
 			includeSList.add(includeNList.item(iClass).getTextContent());
 		}
 		final String[] include = includeSList.toArray(new String[0]);
-		System.out.println("Include files: " + include);
+		Iterator<String> iterator = includeSList.iterator();
+		StringBuilder files= new StringBuilder();
+		while (iterator.hasNext()) {
+			Object next =  iterator.next();
+			files.append(next.toString());
+			if (iterator.hasNext()) {
+				files.append(", ");
+			}
+		}
+		System.out.printf("Include files: %s%n", files);
 
 		final JCodeModel model = new JCodeModel();
 		generatePlumbing(model, nameSpace);
